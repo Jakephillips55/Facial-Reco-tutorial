60ffb7a876e51fe6f4405a82497e7138ec7b160b
Jakephillips55 <Jakep629@gmail.com>
2020-02-28 14:58:20 +0000

    11.22

--- js/whoScript.js
+++ js/whoScript.js
@@ -7,5 +7,21 @@ Promise.all([
 ]).then(start)
 
 function start() {
-  document.body.append('Loaded')
+  const container = document.createElement('div')
+  container.style.position = 'relative'
+  document.body.append(container)
+  document.body.append('Loaded ')
+  imageUpload.addEventListener('change',  async () => {
+    const image = await faceapi.bufferToImage(imageUpload.files[0])
+    document.body.append(image)
+    const canvas = faceapi.createCanvasFromMedia(image)
+    container.append(canvas)
+    const displaySize = { width: image.width, height: image.height }
+    faceapi.matchDimensions(canvas, displaySize)
+    const detections = await faceapi.detectAllFaces(image)
+    .withFaceLandmarks().withFaceDescriptors()
+    const resizedDetections = faceapi.resizeResults(detections, 
+      displaySize)
+      const drawBox = new faceapi.draw.DrawBox(box)
+  })
 }
